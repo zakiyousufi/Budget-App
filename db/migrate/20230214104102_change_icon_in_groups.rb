03b5b3b5d46a5dd16fb08723411ea88cd213b43d
Ahmad zaki Yousufi <ahmadzakiyousufi3@gmail.com>
2023-04-14 23:33:56 +0430

    Delete tiwlindcss and add som style to pages

--- db/migrate/20230214104102_change_icon_in_groups.rb
+++ db/migrate/20230214104102_change_icon_in_groups.rb
@@ -1,5 +1,5 @@
 class ChangeIconInGroups < ActiveRecord::Migration[7.0]
   def change
-    change_column :groups, :icon, :string, default: "https://cdn.dribbble.com/users/9685/screenshots/997495/avatarzzz.gif"
+    change_column :groups, :icon, :string, default: "https://cdn-icons-png.flaticon.com/512/6428/6428645.png"
   end
 end
